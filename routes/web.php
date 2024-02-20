ddb068fc1832df62a58573cf28c38b9959fb7157
vagrant <vagrant@homestead>
2024-02-20 04:13:05 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,8 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 // 博文动态
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+// 关注的人
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+// 粉丝
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
