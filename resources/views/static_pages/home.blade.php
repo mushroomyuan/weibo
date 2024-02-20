cde5e47e17e9315f65c47a1c148424ec1b525e1b
vagrant <vagrant@homestead>
2024-02-20 04:01:06 +0000

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
   @if (Auth::check())
-  <div class="row">
-    <div class="col-md-8">
-      <section class="status_form">
-        @include('shared._status_form')
-      </section>
-      <h4>微博列表</h4>
-      <hr>
-      @include('shared._feed')
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats mt-2">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
+      </aside>
     </div>
-    <aside class="col-md-4">
-      <section class="user_info">
-        @include('shared._user_info', ['user' => Auth::user()])
-      </section>
-    </aside>
-  </div>
   @else
     <div class="bg-light p-3 p-sm-5 rounded">
       <h1>Hello Laravel</h1>
